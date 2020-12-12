c4922204a54e3c9cbd384480a1885099544c41c4
sgs22 <samuel_smith@outlook.com>
2020-12-12 21:07:53 +0000

    fix urls.py and add index card

--- needapc_mvp/urls.py
+++ needapc_mvp/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.conf import settings
+
+from django.conf.urls.static import static
+
 from accounts.views import (
     login_view, 
     logout_view,
@@ -46,4 +50,6 @@ urlpatterns = [
     path('apanel/', admin.site.urls),
 ]
 
-# 2:47:21
+# if settings.DEBUG:
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
